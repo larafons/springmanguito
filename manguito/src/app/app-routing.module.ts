f42ac524261757e4d112739c65d079e9a9b7f3db
Jose Ignacio Borrajo <joseborr8@hotmail.com>
2022-12-14 14:46:27 -0300

    se agregan componentes de login, signin y servicio de users

--- manguito/src/app/app-routing.module.ts
+++ manguito/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent, pathMatch: "full" },
+  { path: "signin", component: SigninComponent, pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
